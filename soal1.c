acaf7ec65fcd51b182dde241493d8200c58afb71
aqilahsafitri <aqilahsafitris@gmail.com>
2026-04-14 08:45:37 +0700

    soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,29 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 1
+ *   Hari dan Tanggal    : Selasa, 14 April 2026
+ *   Nama (NIM)          : Aqilah Safitri Sjarif (13224050)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : cetak warna lampu berdasarkan kondisi
+ */
+
 #include <stdio.h>
 
-int main(void) {
-    return 0;
-}
+int main(){
+    int i;
+    int N;
+    scanf("%d", &i);
+    for (N = 1; N <= i; N++)
+        if (N % 2 == 0 && N % 3 ==0) {
+            printf("BIRU\n");
+        }
+        else if(N % 3 == 0) {
+            printf("MERAH\n");
+        }
+        else if(N % 2 == 0){
+            printf("KUNING\n");
+            }
+        else{
+            printf("%d\n", N);
+            }   
+        return 0;
+    }
